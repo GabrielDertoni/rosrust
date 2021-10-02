8c6d3cfc2c3e22dfd2acc271e898d191f68c3a7f
GabrielDertoni <gab.dertoni@gmail.com>
2021-10-01 21:59:32 -0300

    `new` as create function and use oneshot channel

--- rosrust_async/src/service.rs
+++ rosrust_async/src/service.rs
@@ -1,23 +1,34 @@
+use rosrust::error::Result as RosResult;
+use rosrust::ServicePair;
 use tokio::sync::mpsc;
 
-use rosrust::ServicePair;
-use rosrust::error::Result;
+use crate::oneshot_blocking as oneshot;
 
 pub struct Service<S: ServicePair> {
     raii: rosrust::Service,
-    rx: mpsc::Receiver<RequestHandle<S>>
+    // This should really be a Single Producer, Single Consumer. But there is no such
+    // channel in the Tokio crate.
+    rx: mpsc::Receiver<RequestHandle<S>>,
 }
 
 impl<S: ServicePair> Service<S> {
-    fn new(raii: rosrust::Service, rx: mpsc::Receiver<RequestHandle<S>>) -> Service<S> {
-        Service { raii, rx }
-    }
-}
+    pub fn new(topic: impl AsRef<str>) -> RosResult<Service<S>> {
+        let (tx, rx) = mpsc::channel(1);
 
-impl<S: ServicePair> Service<S> {
-    #[inline]
-    pub fn create(topic: impl AsRef<str>) -> Result<Service<S>> {
-        service(topic)
+        let raii = rosrust::service::<S, _>(
+            topic.as_ref(),
+            move |req: S::Request| -> Result<S::Response, String> {
+                let (response, handle) = RequestHandle::new_pair(req);
+                tx.blocking_send(handle).unwrap();
+
+                match response.recv() {
+                    Ok(resp) => return resp,
+                    Err(_) => panic!("Handle was dropped before responding"),
+                }
+            },
+        )?;
+
+        Ok(Service { raii, rx })
     }
 
     #[inline]
@@ -36,13 +47,13 @@ impl<S: ServicePair> std::ops::Deref for Service<S> {
 
 pub struct RequestHandle<S: ServicePair> {
     request: S::Request,
-    tx: mpsc::Sender<std::result::Result<S::Response, String>>,
+    tx: oneshot::Sender<Result<S::Response, String>>,
 }
 
 impl<S: ServicePair> RequestHandle<S> {
-    fn new_pair(request: S::Request) -> (RequestHandle<S>, mpsc::Receiver<std::result::Result<S::Response, String>>) {
-        let (tx, rx) = mpsc::channel(1);
-        (RequestHandle { tx, request }, rx)
+    fn new_pair(request: S::Request) -> (oneshot::Receiver<Result<S::Response, String>>, RequestHandle<S>) {
+        let (tx, rx) = oneshot::channel();
+        (rx, RequestHandle { request, tx })
     }
 }
 
@@ -55,14 +66,15 @@ impl<S: ServicePair> RequestHandle<S> {
     //       result indicating if the response was send successfully. But in the
     //       current wrapper design, I don't think that's possible.
     pub async fn send_ok(self, response: S::Response) {
-        if let Err(_) = self.tx.send(Ok(response)).await {
+        if let Err(_) = self.tx.send(Ok(response)) {
             panic!("failed to send value");
         }
     }
 
-    pub async fn send_err(self, msg: impl Into<String>) {
-        if let Err(_) = self.tx.send(Err(msg.into())).await {
-            panic!("failed to send err");
+
+    pub fn send_err(self, msg: impl Into<String>) {
+        if let Err(_) = self.tx.send(Err(msg.into())) {
+            panic!("failed to send value");
         }
     }
 }
@@ -71,19 +83,4 @@ impl<S: ServicePair> std::fmt::Debug for RequestHandle<S> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "RequestHandle {{..}}")
     }
-}
-
-pub fn service<S: ServicePair>(topic: impl AsRef<str>) -> Result<Service<S>> {
-    let (tx, rx) = mpsc::channel(1);
-
-    let raii = rosrust::service::<S, _>(
-        topic.as_ref(),
-        move |req: S::Request| -> std::result::Result<S::Response, String> {
-            let (handle, mut response) = RequestHandle::new_pair(req);
-            tx.blocking_send(handle).unwrap();
-            response.blocking_recv().unwrap()
-        }
-    )?;
-
-    Ok(Service::new(raii, rx))
 }
